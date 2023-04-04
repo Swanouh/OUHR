bb12a0528e1b0a55aad590f555a32aabc7ab0fc5
AnyaJade <44472633+AnyaJade@users.noreply.github.com>
2023-04-04 16:02:23 +0100

    Add files via upload

---
+++ ouhr/R/add_plot_excel.R
@@ -0,0 +1,40 @@
+#' Add Plot to Excel
+#'
+#' Adding a R plot to Excel workbook. Plot can be made using `ggplot` or base `R`. Ensure plot exists in the global environment before using the function.
+#'
+#' @param wb A string containing the current workbook.
+#' @param sheetname A string containing the sheetname.
+#' @param plot A plot.
+#' @param plot_position A vector of coordinates of starting column and starting row. Defaults to column `2`, row `1`.
+#' @param plot_width A number for the desired width of plot. Defaults to `8`.
+#' @param plot_height A number for the desired height of plot. Defaults to `5`.
+#'
+#' @return Inserts the plot into the Excel workbook.
+#' @export
+#'
+#' @examples \dontrun{
+#' plot1 <- plot(cars)
+#' wb <- openxlsx::createWorkbook()
+#' openxlsx::addWorksheet(wb, "example")
+#' plot_excel(sheetname = "example", plot = plot1)
+#' save_excel(wb = wb, filename = "C:/Users/Anya.Ferguson/Desktop/example.xlsx")
+#' }
+
+plot_excel <- function(wb = wb, sheetname, plot, plot_position = c(2, 1), plot_width = 8, plot_height = 5){
+
+  if(!missing(plot)){
+
+    print(plot)
+
+    openxlsx::insertPlot(wb,
+               sheetname,
+               startCol = plot_position[1],
+               startRow = plot_position[2],
+               width = plot_width,
+               height = plot_height,
+               fileType = "png",
+               units = "in"
+    )
+  }
+
+}
---
+++ ouhr/R/email_info_request.R
@@ -0,0 +1,117 @@
+#' Creating emails for info requests.
+#'
+#' Depending on the type specified, creates an email to send link to data or password to the requester.
+#'
+#' @param To A string of email addresses separated by "`;`".
+#' @param Cc A string of email addresses separated by "`;`".
+#' @param type Either "`link`", "`password`", "`attachment`" or "`table`" to determine email contents.
+#' @param INF_Number A string.
+#' @param INF_Name A string.
+#' @param dear A string.
+#' @param password A string.
+#' @param filename A string containing filepath and name of attachment using `/` instead of `\`.
+#' @param table A table defined in global environment.
+#'
+#' @return One of two emails, depending on type specified.
+#' @export
+#'
+#' @examples
+#'  \dontrun{email_info_request(type = "password",
+#' To = "email_address",
+#' dear = "email_recipient",
+#' password = "test"
+#' )
+#' }
+
+email_info_request <- function(To, Cc, type, INF_Number = NULL, INF_Name = NULL, dear = NULL, password = NULL, filename = NULL, table = NULL) {
+  # NB: Requires stringr, RDCOMClient
+
+  # Creating an email
+  OutApp <- RDCOMClient::COMCreate("Outlook.Application")
+  outMail <- OutApp$CreateItem(0)
+
+  # Getting the signature
+    # If there are no signatures or multiple, no signature will be pulled into the email
+  outMail$GetInspector()
+  signature <- outMail[["HTMLBody"]]
+
+  # Email recipients
+    # If To and CC not specified, these will be left blank
+  if(!missing(To)){
+    outMail[["To"]] <- To
+  }
+
+  if(!missing(Cc)){
+    outMail[["Cc"]] <- Cc
+  }
+
+  # Body, subject and attachments of message depends on the type of email.
+    # PID info requests will have one email to send out the link to the data and another to send out the password.
+    # Non PID info requests can either have an email to send out an attachment or an email to send out a table in the body of the email.
+  if(type == "link"){
+    outMail[["subject"]] <- paste(INF_Number, "-", INF_Name)
+
+    outMail[["HTMLBody"]] <- paste0('<p>',
+                                    stringr::str_replace("Dear X,", "X", dear),
+                                    '<p>',
+                                    "Please find the data in: INSERT LINK HERE",
+                                    '<p>',
+                                    "The password will follow in another email. This information will be available for a period of 4 weeks; please ensure that the data is used or stored safely if you need the data after this time. You may need to log off and then back on for access to the file.",
+                                    '<p>',
+                                    "Please let me know if you have any questions.",
+                                    signature,
+                                    '</p>'
+    )
+  }
+
+  if(type == "password"){
+    outMail[["HTMLBody"]] <- paste0('<p>',
+                                    stringr::str_replace("Dear X,", "X", dear),
+                                    '<p>',
+                                    "P: ",
+                                    password,
+                                    '<p>',
+                                    signature,
+                                    '</p>'
+                                    )
+
+    }
+
+  if(type == "attachment"){
+    outMail[["HTMLBody"]] <- paste0('<p>',
+                                    stringr::str_replace("Dear X,", "X", dear),
+                                    '<p>',
+                                    "Please find the data attached.",
+                                    '<p>',
+                                    "Please let me know if you have any questions.",
+                                    signature,
+                                    '</p>'
+                                    )
+
+      if(file.exists(filename)){
+        outMail[["Attachments"]]$Add(filename)
+      }
+
+      if(!file.exists(filename)){
+        stop("Attachment not found. Please check that the file you wish to attach exists and that you have provided the correct filename and used /.")
+      }
+    }
+
+  if(type == "table"){
+    outMail[["HTMLBody"]] <- paste0('<p>',
+                                    stringr::str_replace("Dear X,", "X", dear),
+                                    '<p>',
+                                    "Please find the data in the table below.",
+                                    '<p>',
+                                    table,
+                                    '<p>',
+                                    "Please let me know if you have any questions.",
+                                    signature,
+                                    '</p>'
+                                    )
+    }
+
+  # Opens email in new window so can review before sending:
+  outMail$Display()
+
+}
