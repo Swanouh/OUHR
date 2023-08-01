fba401190997be2dea6b7e70e56ce44910f33a3e
AnyaJade <44472633+AnyaJade@users.noreply.github.com>
2023-08-01 15:20:04 +0100

    Removing mistake

--- ouhr/ouhr/tests/testthat/test-financial_year.R
+++
@@ -1,7 +0,0 @@
-test_that("financial year returns the financial year", {
-  fy <- financial_year(as.Date("2023-03-31"))
-  fy2 <- financial_year(as.Date("2023-04-01"))
-
-  expect_equal(fy, "2022-2023")
-  expect_equal(fy2, "2023-2024")
-})
--- ouhr/ouhr/tests/testthat/test-previous_month.R
+++
@@ -1,7 +0,0 @@
-test_that("previous_month returns the previous month", {
-  pm <- previous_month(Sys.Date())
-
-  month_pm <- lubridate::month(pm)
-
-  expect_equal(month_pm, lubridate::month(Sys.Date())-1)
-})
