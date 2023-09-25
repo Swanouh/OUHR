dc4193ae26fe52ac54b823b30cb991b5b55b1376
AnyaJade <44472633+AnyaJade@users.noreply.github.com>
2023-09-25 14:03:09 +0100

    Adding wrap_up and news.md

---
+++ ouhr/NEWS.md
@@ -0,0 +1,4 @@
+# ouhr 0.1.0
+
+* Added a `NEWS.md` file to track changes to the package.
+* Added `wrap-up.R` as part of INF-26712.
---
+++ ouhr/R/wrap_up.R
@@ -0,0 +1,37 @@
+#' Wrap up
+#'
+#' A function to print the run-time of the Rmd to the html output and to log start and end datetimes to Info.Adhoc.R_Procedures.
+#'
+#' @return Run-time of Rmd.
+#' @export
+#'
+#' @examples \dontrun{procedure_start_time <- Sys.Date()
+#' wrap_up()}
+
+wrap_up <- function(){
+
+  procedure_end_time <- Sys.time()
+
+  connection_WH <- DBI::dbConnect(odbc::odbc(),
+                                  Driver = "SQL Server",
+                                  Server = "oxnetorbit01\\tdwh_live",
+                                  Database = "Info"
+  )
+
+  title <- rmarkdown::metadata$title
+
+  log <- data.frame("StartDateTime" = as.character(procedure_start_time),
+                    "EndDateTime" = as.character(procedure_end_time),
+                    "RScriptName" = title
+  )
+
+  DBI::dbAppendTable(conn = connection_WH,
+                     name = DBI::SQL("Info.Adhoc.R_Procedures"),
+                     value = log
+  )
+
+  paste("Runtime:",
+        hms::round_hms(hms::as_hms(difftime(procedure_end_time, procedure_start_time)), 1)
+  )
+
+}
